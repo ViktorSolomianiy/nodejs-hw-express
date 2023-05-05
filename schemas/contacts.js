f62a62b797a170ca9be2e532d7cac6d3dffbee8a
ViktorSolomianiy <xmegasystem35@gmail.com>
2023-05-05 20:06:33 +0300

    fix: додав схему для оновлення контакту

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -6,4 +6,10 @@ const addSchema = Joi.object({
   phone: Joi.string().required(),
 });
 
-module.exports = { addSchema };
+const updateSchema = Joi.object({
+  name: Joi.string(),
+  email: Joi.string(),
+  phone: Joi.string(),
+});
+
+module.exports = { addSchema, updateSchema };
